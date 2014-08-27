5da61bb6723f89653e7b890fb162c8a84b4ac8ef
smith200791 <macbookretina@localhost>
2014-08-27 19:51:43 +0400

    пошла родимая!

--- Calc/src/Main.java
+++ Calc/src/Main.java
@@ -1,4 +1,3 @@
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 
@@ -7,13 +6,7 @@ public class Main {
 	public static void main(String[] args) {
 		Calculate calculate = new Calculate();
 		calculate.ssumm(2, 2);
-		calculate.viewLastResult("console");
-        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext(new String[] {"config.xml"});//показываем Spring где лежит файл конфигурации
-        //Также вместо ClassPathXmlApplicationContext можно использовать FileSystemXmlApplicationContext, который используется абсолютно также с той разнице, что путь к config.xml нужно будет указывать полностью. 
-        
-//        Hello h = (Hello) ac.getBean("hello");//указываем id нашего bean-а
-//        h.sayHi();
-
+		calculate.viewLastResult("superConsole");
 	}
 
 	
