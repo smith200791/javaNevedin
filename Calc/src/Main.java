b25a7c52af34a8bfd44c909490b4a75797b377c2
smith200791 <macbookretina@172.20.10.2>
2014-09-01 10:02:03 +0400

    + ПАРУ ФАЙЛОВ

--- Calc/src/Main.java
+++ Calc/src/Main.java
@@ -24,7 +24,6 @@ public class Main {
        
         sessionFactory = createSessionFactory();
         Session session = sessionFactory.openSession();
-
         List<CalcOperations> calcOperations = (List<CalcOperations>) session.createQuery("from Book order by name").list();
 
     }
