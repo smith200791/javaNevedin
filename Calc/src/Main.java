cab6d3cefd39f59f1ed9e1fc448569cae40ccbf9
smith200791 <macbookretina@172.20.10.2>
2014-09-01 10:00:48 +0400

    ПРИКРУТИЛ ХИБЕРНЕЙТ ЕЩЕ НЕ ТЕСТИЛ, ПРОСТО КОМИЧУ

--- Calc/src/Main.java
+++ Calc/src/Main.java
@@ -1,17 +1,40 @@
+import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
+import database.CalcOperations;
 
 public class Main {
-	
-	public static void main(String[] args) {
-		Calculate calculate = new Calculate();
-		calculate.ssumm(2, 2);
-		calculate.viewLastResult("superConsole");
-		
-		calculate.executeOperation("multiply", 2,3);
+
+    private static SessionFactory sessionFactory;
+    private static ServiceRegistry serviceRegistry;
+
+    public static void main(String[] args) {
+
+        Calculate calculate = new Calculate();
+        calculate.ssumm(2, 2);
         calculate.viewLastResult("superConsole");
 
-	}
+        calculate.executeOperation("multiply", 2, 3);
+        calculate.viewLastResult("superConsole");
+       
+        sessionFactory = createSessionFactory();
+        Session session = sessionFactory.openSession();
 
-	
+        List<CalcOperations> calcOperations = (List<CalcOperations>) session.createQuery("from Book order by name").list();
+
+    }
+
+    public static SessionFactory createSessionFactory() {
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        return sessionFactory;
+    }
+
 }
--- Calc/src/factory/OperationFactory.java
+++ Calc/src/factory/OperationFactory.java
@@ -7,7 +7,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class OperationFactory {
 	@SuppressWarnings("resource")
 	public Operation createOperation(String code,Integer integer,Integer integer2) {
-		ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext(new String[] {"config.xml"});//показываем Spring где лежит файл конфигурации
+		ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext(new String[] {"config.xml"});
+		//показываем Spring где лежит файл конфигурации
         return (Operation)ac.getBean(code);
 	}
 }
