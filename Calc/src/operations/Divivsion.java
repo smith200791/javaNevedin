b4d56e25568cc86c16743b0c8cf6e39baa8e87e4
smith200791 <macbookretina@172.20.10.2>
2014-09-11 20:20:05 +0400

    test

--- Calc/src/operations/Divivsion.java
+++ Calc/src/operations/Divivsion.java
@@ -5,6 +5,7 @@ import intf.Operation;
 public class Divivsion implements Operation {
 
     @Override
+    
     public Double operate(Double firstAgr, Double seconArg) {
         if (seconArg != null && firstAgr != null && seconArg != 0) {
             return (double) (firstAgr / seconArg);
